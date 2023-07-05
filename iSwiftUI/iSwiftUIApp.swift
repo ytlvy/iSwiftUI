7a99f73facb343d7fd68f825496ea574d2bdaaee
gyj <yanjie.guo@kuwo.cn>
2023-07-05 22:40:13 +0800

    Initial Commit

---
+++ iSwiftUI/iSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iSwiftUIApp.swift
+//  iSwiftUI
+//
+//  Created by admin on 2023/7/5.
+//
+
+import SwiftUI
+
+@main
+struct iSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
